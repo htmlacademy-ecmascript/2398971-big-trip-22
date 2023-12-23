9ec79b47ffe048dfed6068fdead71667902e58c1
MrScience969 <scieence969@gmail.com>
2023-12-23 20:00:33 +0700

    Выделит показ задач в функцию.

--- src/presenter/trip-presenter.js
+++ src/presenter/trip-presenter.js
@@ -30,23 +30,28 @@ export default class TripPresenter {
 
     //render(new EventNewView(), this.#eventListComponent.element);
 
-    render(new EditingEventView({
-      eventPoint: this.#pointsModel[0],
-      eventOffers: this.#offersModel.getOfferByType(this.#pointsModel[0].type),
-      eventDestination: this.#destinationsModel.getDestinationById(this.#pointsModel[0].destination),
-      allOffers: this.#allOffers,
-      allDestinations: this.#allDestinations,
-    }),
-    this.#eventListComponent.element);
+    // render(new EditingEventView({
+    //   eventPoint: this.#pointsModel[0],
+    //   eventOffers: this.#offersModel.getOfferByType(this.#pointsModel[0].type),
+    //   eventDestination: this.#destinationsModel.getDestinationById(this.#pointsModel[0].destination),
+    //   allOffers: this.#allOffers,
+    //   allDestinations: this.#allDestinations,
+    // }),
+    // this.#eventListComponent.element);
 
 
-    for (let i = 1; i < this.#pointsModel.length; i++) {
-      render(new EventPointView({
+    for (let i = 0; i < this.#pointsModel.length; i++) {
+      this.#renderEventPoint({
         eventPoint: this.#pointsModel[i],
         eventOffers: this.#offersModel.getOfferById(this.#pointsModel[i].type , this.#pointsModel[i].offers),
         eventDestination: this.#destinationsModel.getDestinationById(this.#pointsModel[i].destination),
-      }),
-      this.#eventListComponent.element);
+      });
     }
+  }
+
+  #renderEventPoint({eventPoint, eventOffers, eventDestination }) {
+    const eventPointComponent = new EventPointView({eventPoint, eventOffers, eventDestination});
+
+    render(eventPointComponent, this.#eventListComponent.element);
   }
 }
