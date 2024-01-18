61f67b5a020578ff7555e818e3db853fea80e36c
MrScience969 <scieence969@gmail.com>
2024-01-18 20:13:49 +0700

    Добавит презентер фильтров

--- src/main.js
+++ src/main.js
@@ -1,17 +1,10 @@
 import { render } from './framework/render.js';
-import FilterView from './view/filter-view.js';
 import TripPresenter from './presenter/trip-presenter.js';
+import FilterPresenter from './presenter/filter-presenter.js';
 import PointsModel from './model/points-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
 import FilterModel from './model/filter-model.js';
-
-const filters = [
-  {
-    type: 'all',
-    count: 0,
-  },
-];
 
 const siteHeaderElement = document.querySelector('.page-header');
 const siteFilterElement = siteHeaderElement.querySelector('.trip-controls__filters');
@@ -22,6 +15,12 @@ const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
 const filterModel = new FilterModel();
 
+const filterPresenter = new FilterPresenter({
+  filterContainer: siteFilterElement,
+  filterModel,
+  pointsModel
+});
+
 const tripPresenter = new TripPresenter({
   pointContainer: siteEventsElement,
   pointsModel,
@@ -29,10 +28,5 @@ const tripPresenter = new TripPresenter({
   destinationsModel
 });
 
-render(new FilterView({
-  filters,
-  currentFilterType: 'all',
-  onFilterTypeChange: () => {}
-}), siteFilterElement);
-
+filterPresenter.init();
 tripPresenter.init();
