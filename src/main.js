ed0442ab46d4cdec22bf5061aa3b7f53472403cb
MrScience969 <scieence969@gmail.com>
2024-01-18 19:52:43 +0700

    Добавит модель для фильтра. WIP

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { render } from './framework/render.js';
+import { generateFilter } from './mock/filter.js';
 import FilterView from './view/filter-view.js';
 import TripPresenter from './presenter/trip-presenter.js';
-//import TripInfoView from './view/trip-info-view.js';
 import PointsModel from './model/points-model.js';
 import OffersModel from './model/offers-model.js';
 import DestinationsModel from './model/destinations-model.js';
-import { generateFilter } from './mock/filter.js';
+import FilterModel from './model/filter-model.js';
 
 const siteHeaderElement = document.querySelector('.page-header');
 const siteFilterElement = siteHeaderElement.querySelector('.trip-controls__filters');
@@ -15,7 +15,7 @@ const siteEventsElement = document.querySelector('.trip-events');
 const pointsModel = new PointsModel();
 const offersModel = new OffersModel();
 const destinationsModel = new DestinationsModel();
-
+const filterModel = new FilterModel();
 
 const tripPresenter = new TripPresenter({
   pointContainer: siteEventsElement,
