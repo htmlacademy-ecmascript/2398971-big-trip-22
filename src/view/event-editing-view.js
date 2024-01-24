5dc6f3a6055a4e2b87650dac4771af4de6bb4b59
MrScience969 <scieence969@gmail.com>
2024-01-24 20:04:38 +0700

    Реализоват отрисовку скролл бара

--- src/view/event-editing-view.js
+++ src/view/event-editing-view.js
@@ -96,12 +96,12 @@ function createSectionDestination(eventDestination) {
       <h3 class="event__section-title  event__section-title--destination">Destination</h3>
       <p class="event__destination-description">${eventDestination.description}</p>
 
-      <div class="event__photos-container">
+      ${eventDestination.pictures.length > 0 ? `<div class="event__photos-container">
         <div class="event__photos-tape">
           ${eventDestination.pictures.map((picture) =>`
           <img class="event__photo" src=${picture.src} alt="Event photo">`).join('')}
         </div>
-      </div>
+      </div>` : ''}
 
     </section>`;
 }
